db7facbd86788eb20172a00eac610d6718b694d3
kevmoffo <kevinkuete@gmail.com>
2016-08-29 14:35:09 -0400

    Création du script de contrainte du package Contrat

---
+++ Sprint 1/Package 1/BD/Test/CTE_Pack_Contrat.sql
@@ -0,0 +1,42 @@
+--But: Création des contraintes du package Contrat.
+--Date:29-08-2016
+--Par: Kevin Kuété Moffo
+
+
+Use PE2_Test
+GO
+ Print 'Création des Contraintes de la table tblContrat'
+
+ Alter table S_Contrat.tblContrat
+ Alter Column noContrat  varchar(20) NOT NULL
+
+ Alter table S_Contrat.tblContrat
+ Alter Column nom  varchar(20) NOT NULL
+
+ Print 'Création de la PK et des Fk de la table tblContrat'
+ Alter table S_Contrat.tblContrat ADD
+ Constraint CON_PK
+ Primary Key(noContrat) 
+
+ Alter table S_Contrat.tblContrat ADD
+ Constraint CON_AGE_FK
+ Foreign Key(noAgence) 
+ References S_Contrat.tblAgence(noAgence)
+
+ Alter table S_Contrat.tblContrat ADD
+ Constraint CON_STA_FK
+ Foreign Key(noStatut) 
+ References S_Contrat.tblStatut(noStatut)
+
+ GO
+  Print 'Création des Contraintes de la table tblStatut'
+ Alter table S_Contrat.tblStatut
+ Alter Column noStatut  Int NOT NULL
+ Alter table S_Contrat.tblStatut
+ Alter Column nomStatut  varchar(20) NOT NULL
+
+
+
+
+
+
