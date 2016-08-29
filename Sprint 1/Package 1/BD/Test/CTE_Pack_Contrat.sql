dcfc0559c12def54e0b939543744bd0aea7a9412
kevmoffo <kevinkuete@gmail.com>
2016-08-29 15:24:37 -0400

    une modification du scritp de contrainte

--- Sprint 1/Package 1/BD/Test/CTE_Pack_Contrat.sql
+++ Sprint 1/Package 1/BD/Test/CTE_Pack_Contrat.sql
@@ -5,42 +5,43 @@
 
 Use PE2_Test
 GO
- Print 'Création des Contraintes de la table tblContrat'
+ PRINT 'Création des Contraintes de la TABLE tblContrat'
 
- Alter table S_Contrat.tblContrat
- Alter Column noContrat  varchar(20) NOT NULL
+ ALTER TABLE S_Contrat.tblContrat
+ ALTER Column noContrat  varchar(20) NOT NULL
 
- Alter table S_Contrat.tblContrat
- Alter Column nom  varchar(20) NOT NULL
+ ALTER TABLE S_Contrat.tblContrat
+ ALTER Column nom  varchar(20) NOT NULL
+ GO
 
- Print 'Création de la PK et des Fk de la table tblContrat'
- Alter table S_Contrat.tblContrat ADD
- Constraint CON_PK
- Primary Key(noContrat) 
+ PRINT 'Création de la PK et des Fk de la TABLE tblContrat'
+ ALTER TABLE S_Contrat.tblContrat ADD
+ CONSTRAINT CON_PK
+ PRIMARY Key(noContrat) 
 
- Alter table S_Contrat.tblContrat ADD
- Constraint CON_AGE_FK
- Foreign Key(noAgence) 
+ ALTER TABLE S_Contrat.tblContrat ADD
+ CONSTRAINT CON_AGE_FK
+ FOREIGN Key(noAgence) 
  References S_Contrat.tblAgence(noAgence)
 
- Alter table S_Contrat.tblContrat ADD
- Constraint CON_STA_FK
- Foreign Key(noStatut) 
+ ALTER TABLE S_Contrat.tblContrat ADD
+ CONSTRAINT CON_STA_FK
+ FOREIGN Key(noStatut) 
  References S_Contrat.tblStatut(noStatut)
 
  GO
- Print 'Création des Contraintes de la table tblStatut'
- Alter table S_Contrat.tblStatut
- Alter Column noStatut  Int NOT NULL
- Alter table S_Contrat.tblStatut
- Alter Column nomStatut  varchar(20) NOT NULL
+ PRINT 'Création des Contraintes de la TABLE tblStatut'
+ ALTER TABLE S_Contrat.tblStatut
+ ALTER Column noStatut  Int NOT NULL
+ ALTER TABLE S_Contrat.tblStatut
+ ALTER Column nomStatut  varchar(20) NOT NULL
 
  GO
- Print 'Création des Contraintes de la table tblAgence'
- Alter table S_Contrat.tblAgence
- Alter Column noAgence  varchar(15) NOT NULL
- Alter table S_Contrat.tblStatut
- Alter Column nomAgence  varchar(20) NOT NULL
+ PRINT 'Création des Contraintes de la TABLE tblAgence'
+ ALTER TABLE S_Contrat.tblAgence
+ ALTER Column noAgence  varchar(15) NOT NULL
+ ALTER TABLE S_Contrat.tblStatut
+ ALTER Column nomAgence  varchar(20) NOT NULL
 
 
 
