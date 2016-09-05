379d513d188ccc5f4c2fa2a048ce8de6c2b3dd44
Raphael <cotlarrc@msn.com>
2016-09-05 18:06:32 -0400

    Début de programmation du pompage de données de la BD

---
+++ Sprint 1/Package 1/Code/Source/FestiRire/Modele/ModeleElement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestiRire.Modele
+{
+    abstract class ModeleElement
+    {
+        public bool Enregistrer()
+        {
+            if (this.Existe())
+            {
+                return this.Update();
+            }
+            else
+            {
+                return this.Create();
+            }
+        }
+
+        abstract protected bool Existe();
+
+        abstract protected bool Update();
+
+        abstract protected bool Create();
+        abstract public bool Supprimer();
+    }
+}
