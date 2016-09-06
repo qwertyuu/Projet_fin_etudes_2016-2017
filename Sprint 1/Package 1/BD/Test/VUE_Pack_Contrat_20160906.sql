ec83954997b6b3b30d303546c8b2246f64f2c35f
kevmoffo <kevinkuete@gmail.com>
2016-09-06 12:40:16 -0400

    Finalisation des vues de sommaires

--- Sprint 1/Package 1/BD/Test/VUE_Pack_Contrat_20160906.sql
+++ Sprint 1/Package 1/BD/Test/VUE_Pack_Contrat_20160906.sql
@@ -6,7 +6,7 @@ USE PE2_Test
 
 GO
 
-CREATE VIEW  S_Contrat.vueContrat AS
+CREATE VIEW  S_Contrat.vueSomContrat AS
 SELECT con.noContrat, agen.nom,sta.nomStatut, con.lieu
 FROM S_Contrat.tblAgence AS agen 
 JOIN S_Contrat.tblContrat AS con 
@@ -16,4 +16,26 @@ ON con.noStatut=sta.noStatut
 PRINT 'Création de la vue reliée a l''affichage du sommaire contrat'
 GO
 
-PRINT 'Création de la vue reliée a l''affichage du sommaire contrat'
+CREATE VIEW  S_Contrat.vueSomCatArtiste AS
+SELECT cat.nom, cat.description
+FROM S_Contrat.tblCategorieArtiste AS cat
+PRINT 'Création de la vue reliée a l''affichage du sommaire catégories d''artistes'
+GO
+
+CREATE VIEW  S_Contrat.vueSomArtiste AS
+SELECT Art.nom as 'nomAriste',Cat.nom as 'nomCat'
+FROM S_Contrat.tblArtiste AS Art
+JOIN S_Contrat.tblCatArtisteArt AS catArt
+ON Art.noArtiste=catArt.noArtiste
+JOIN S_Contrat.tblCategorieArtiste AS Cat
+ON catArt.noCategorie=Cat.noCategorie
+PRINT 'Création de la vue pour sommaire artiste'
+GO
+
+CREATE VIEW  S_Contrat.vueSomAgence AS
+SELECT agen.nom, agen.ville, agen.telBureau,agen.telCellulaire
+FROM S_Contrat.tblAgence AS agen
+PRINT 'Création de la vue pour sommaire agence'
+
+
+
