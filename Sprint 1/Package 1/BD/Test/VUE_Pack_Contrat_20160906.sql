25bd4b1c608ba88c269be979f701884808a58e33
Raphael <cotlarrc@msn.com>
2016-09-06 21:19:25 -0400

    Ajout de clés primaires aux vues

--- Sprint 1/Package 1/BD/Test/VUE_Pack_Contrat_20160906.sql
+++ Sprint 1/Package 1/BD/Test/VUE_Pack_Contrat_20160906.sql
@@ -18,14 +18,14 @@ PRINT 'Création de la vue reliée a l''affichage du sommaire contrat'
 
 GO
 CREATE VIEW  S_Contrat.vueSomCatArtiste AS
-SELECT cat.nom, cat.description
+SELECT cat.noCategorie, cat.nom, cat.description
 FROM S_Contrat.tblCategorieArtiste AS cat
 GO
 PRINT 'Création de la vue reliée a l''affichage du sommaire catégories d''artistes'
 GO
 
 CREATE VIEW  S_Contrat.vueSomArtiste AS
-SELECT Art.nom as 'nomAriste',Cat.nom as 'nomCat'
+SELECT Art.noArtiste, Art.nom as 'nomAriste',Cat.nom as 'nomCat'
 FROM S_Contrat.tblArtiste AS Art
 JOIN S_Contrat.tblCatArtisteArt AS catArt
 ON Art.noArtiste=catArt.noArtiste
@@ -36,14 +36,14 @@ PRINT 'Création de la vue pour sommaire artiste'
 GO
 
 CREATE VIEW  S_Contrat.vueSomAgence AS
-SELECT agen.nom, agen.ville, agen.telBureau,agen.telCellulaire
+SELECT agen.noAgence, agen.nom, agen.ville, agen.telBureau,agen.telCellulaire
 FROM S_Contrat.tblAgence AS agen
 GO
 PRINT 'Création de la vue pour sommaire agence'
 GO
 
 CREATE VIEW  S_Contrat.vueSomEngagement AS
-SELECT eng.nature, eng.date, eng.heure, eng.duree, eng.lieu, con.noContrat
+SELECT eng.noEngagement, eng.nature, eng.date, eng.heure, eng.duree, eng.lieu, con.noContrat
 FROM S_Contrat.tblEngagement AS eng
 JOIN S_Contrat.tblContrat AS con ON con.noContrat = eng.noContrat
 GO
@@ -51,7 +51,7 @@ PRINT 'Création de la vue pour sommaire Engagement'
 GO
 
 CREATE VIEW  S_Contrat.vueSomExigence AS
-SELECT exi.nom, exi.date, exi.montant, exi.descriptionCourte, exi.noStatut, con.noContrat
+SELECT exi.noExigence, exi.nom, exi.date, exi.montant, exi.descriptionCourte, exi.noStatut, con.noContrat
 FROM S_Contrat.tblExigence AS exi
 JOIN S_Contrat.tblContrat AS con ON con.noContrat = exi.noContrat
 GO
