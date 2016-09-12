02eb4dba2303c444975c4314393e72278c701937
Simon <girsi1330364@etu.cegepjonquiere.ca>
2016-09-11 21:33:24 -0400

    Ajout de la vue des sommaires des statuts + ajout du WHERE datesupprim a toutes les autres vues

--- Sprint 1/Package 1/BD/Officielle/VUE_Pack_Contrat_20160906.sql
+++ Sprint 1/Package 1/BD/Officielle/VUE_Pack_Contrat_20160906.sql
@@ -7,55 +7,70 @@ USE PE2_Officiel
 GO
 
 CREATE VIEW  S_Contrat.vueSomContrat WITH SCHEMABINDING AS
-SELECT con.noContrat, agen.nom,sta.nomStatut, con.lieu
+SELECT con.noContrat, agen.nom,sta.nomStatut, con.lieu, con.dateSupprime
 FROM S_Contrat.tblAgence AS agen 
 JOIN S_Contrat.tblContrat AS con 
 ON con.noAgence=agen.noAgence
 JOIN S_Contrat.tblStatut AS sta
 ON con.noStatut=sta.noStatut
+WHERE con.dateSupprime = null
 GO
 PRINT 'Création de la vue reliée a l''affichage du sommaire contrat'
 
 GO
 CREATE VIEW  S_Contrat.vueSomCatArtiste WITH SCHEMABINDING AS
-SELECT cat.noCategorie, cat.nom, cat.description
+SELECT cat.noCategorie, cat.nom, cat.description, cat.dateSupprime
 FROM S_Contrat.tblCategorieArtiste AS cat
+WHERE cat.dateSupprime = null
 GO
 PRINT 'Création de la vue reliée a l''affichage du sommaire catégories d''artistes'
 GO
 
 CREATE VIEW  S_Contrat.vueSomArtiste WITH SCHEMABINDING AS
-SELECT Art.noArtiste, Art.nom as 'nomAriste',Cat.nom as 'nomCat'
+SELECT Art.noArtiste, Art.nom as 'nomAriste',Cat.nom as 'nomCat',Art.dateSupprime
 FROM S_Contrat.tblArtiste AS Art
 JOIN S_Contrat.tblCatArtisteArt AS catArt
 ON Art.noArtiste=catArt.noArtiste
 JOIN S_Contrat.tblCategorieArtiste AS Cat
 ON catArt.noCategorie=Cat.noCategorie
+WHERE Art.dateSupprime = null
 GO
 PRINT 'Création de la vue pour sommaire artiste'
 GO
 
 CREATE VIEW  S_Contrat.vueSomAgence WITH SCHEMABINDING AS
-SELECT agen.noAgence, agen.nom, adre.ville, adre.telBureau, adre.telCellulaire
+SELECT agen.noAgence, agen.nom, adre.ville, adre.telBureau, adre.telCellulaire, adre.dateSupprime
 FROM S_Contrat.tblAgence AS agen
 JOIN S_Contrat.tblAdresse AS adre
 ON agen.noAdresse=adre.noAdresse
+WHERE adre.dateSupprime = null
 GO
 PRINT 'Création de la vue pour sommaire agence'
 GO
 
 CREATE VIEW  S_Contrat.vueSomEngagement WITH SCHEMABINDING AS
-SELECT eng.noEngagement, eng.nature, eng.date, eng.heure, eng.duree, eng.lieu, con.noContrat
+SELECT eng.noEngagement, eng.nature, eng.date, eng.heure, eng.duree, eng.lieu, eng.dateSupprime, con.noContrat
 FROM S_Contrat.tblEngagement AS eng
 JOIN S_Contrat.tblContrat AS con ON con.noContrat = eng.noContrat
+WHERE eng.dateSupprime = null
 GO
 PRINT 'Création de la vue pour sommaire Engagement'
 GO
 
 CREATE VIEW  S_Contrat.vueSomExigence WITH SCHEMABINDING AS
-SELECT exi.noExigence, exi.nom, exi.date, exi.montant, exi.descriptionCourte, exi.noStatut, con.noContrat
+SELECT exi.noExigence, exi.nom, exi.date, exi.montant, exi.descriptionCourte, exi.noStatut, exi.dateSupprime, con.noContrat
 FROM S_Contrat.tblExigence AS exi
 JOIN S_Contrat.tblContrat AS con ON con.noContrat = exi.noContrat
+WHERE exi.dateSupprime = null
 GO
 PRINT 'Création de la vue pour sommaire Exigence'
+GO
+
+CREATE VIEW S_Contrat.vueSomStatut WITH SCHEMABINDING AS
+SELECT sta.noStatut, sta.NomStatut, sta.description, sta.couleur, sta.dateSupprime
+FROM S_Contrat.tblStatut AS sta
+JOIN S_Contrat.tblContrat AS con ON con.noStatut = sta.noStatut
+WHERE con.noStatut != sta.noStatut AND sta.dateSupprime = null
+GO
+PRINT 'Création de la vue pour sommaire Statut'
 GO
