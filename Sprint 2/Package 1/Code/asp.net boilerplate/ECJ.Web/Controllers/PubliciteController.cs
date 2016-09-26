6bd26ba25fd3eb544940839e04cad97de7aaaecd
kevmoffo <kevinkuete@gmail.com>
2016-09-26 13:10:23 -0400

    Créatrion de l'interface et du controller pour la publicité.

---
+++ Sprint 2/Package 1/Code/asp.net boilerplate/ECJ.Web/Controllers/PubliciteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECJ.Web.Controllers
+{
+    public class PubliciteController : ECJControllerBase
+    {
+        // GET: Publicite
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
