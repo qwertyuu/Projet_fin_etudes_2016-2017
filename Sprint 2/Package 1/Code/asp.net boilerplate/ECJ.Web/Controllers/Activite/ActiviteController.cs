5b510b2a78c22ff45d32aabb461e7196549234c2
qwertyuu <cotlarrc@gmail.com>
2016-10-16 22:21:09 -0400

    Appeler ajout d'Activite depuis le sous-evenement

--- Sprint 2/Package 1/Code/asp.net boilerplate/ECJ.Web/Controllers/Activite/ActiviteController.cs
+++ Sprint 2/Package 1/Code/asp.net boilerplate/ECJ.Web/Controllers/Activite/ActiviteController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ECJ.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,8 +16,18 @@ namespace ECJ.Web.Controllers.Activite
             return View();
         }
 
-        public ActionResult Ajout()
+        public ActionResult Ajout(int? evenement_id, int? sousevenement_id)
         {
+            if (evenement_id == null && sousevenement_id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            return View(new Tuple<int?, int?>(evenement_id, sousevenement_id));
+        }
+        [HttpPost]
+        public ActionResult Ajout(tblActivite tblActivite)
+        {
+            //traiter l'Ajout
             return View();
         }
 
