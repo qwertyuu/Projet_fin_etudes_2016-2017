033e083f56cd273c2d69f75166e7653d39f1d87c
Simon <girsi1330364@etu.cegepjonquiere.ca>
2016-10-17 16:01:23 -0400

    Avancement du calculateur; avancement du calcul de âge, changement de l'interface des âges

--- Sprint 2/Package 1/Code/asp.net boilerplate/ECJ.Web/Views/Calculateur/Index.js
+++ Sprint 2/Package 1/Code/asp.net boilerplate/ECJ.Web/Views/Calculateur/Index.js
@@ -1,15 +1,15 @@
 var nbBillet;
 var nbBilletVIP;
-var TotalMontant;
 var TotalBillet;
-
+var PrixMoyen;
 function calculTotalBillet() {
     return TotalBillet = parseInt(nbBillet) + parseInt(nbBilletVIP);
 } //Met a jour le nombre de billet entré et le retourne 
 
-function calculTotalMontant(montantAdditionel) {
-    TotalMontant = TotalMontant + montantAdditionel;
-} //n'est pas utilisé ce n'est pas un bon moyen finalement je doit trouver une alternative
+function calculMoyenne() {
+    PrixMoyen = parseInt($("#totalBillet").val()) + parseInt($("#totalBilletVIP").val());
+    PrixMoyen = PrixMoyen / calculTotalBillet();
+} //Calcule la moyenne des billets pour le calcul des âges
 
 function calculBillet() {
 
@@ -40,15 +40,28 @@ function calculBilletVIP() {
 } //Calcule le prix des billets VIP
 
 function pourcentCheck() {
-    if ($("#JeunePourcent").val() >= 101 || $("#JeunePourcent").val() <= -1 || $("#AdultesPourcent").val() >= 101 || $("#AdultesPourcent").val() <= -1 || $("#AinePourcent").val() >= 101 || $("#AinePourcent").val() <= -1) {
+    if ($("#JeunePourcent").val() > 101 || $("#JeunePourcent").val() < -1 || $("#AdultePourcent").val() > 101 || $("#AdultePourcent").val() < -1 || $("#AinePourcent").val() > 101 || $("#AinePourcent").val() < -1) {
         alert("Veuiller entrer un pourcentage de rabais valide.");
 
         return false
     }
+    //if (parseInt($("#RatioJeune").val()) + parseInt($("#RatioAdulte").val()) + parseInt($("#RatioAine").val()) == 100) {
+        
+    //}
     else {
         return true
     }
 } //Vérifie que les % des âges sont Positif et moins que 100 et retourne un bool
+
+function ratioCheck() {
+    if ($("#RatioJeune").val() > -1 && $("#RatioJeune").val() < 101 || $("#RatioAdulte").val() > -1 && $("#RatioAdulte").val() < 101 || $("#RatioAdulte").val() > -1 && $("#RatioAine").val() < 101) {
+        if (parseInt($("#RatioJeune").val()) + parseInt($("#RatioAdulte").val()) + parseInt($("#RatioAine").val()) == 100) {
+            return true
+        }
+    }
+    alert("Veuiller entrer un ratio entre 0 et 100 seulement");
+    return false
+} //Vérifie que les ratios entrés soient entre 0 et 100 pour ensuite vérifier si additionnés ils = 100 ensuite retourne un bool
 
 function positifBilletCheck() {
     if ($("#nbBillet").val() <= -1 || $("#nbBilletVIP").val() <= -1) {
@@ -76,9 +89,24 @@ function calculSouperSpectacle() {
 } //Calcule le prix des souper spectacle après avoir vérifier que les données entrées soient bonnes
 
 function calculRabaisAge() {
-    if (pourcentCheck() == true && $("#nbBilletJeune").val() > TotalBillet) {
-        var Jeune = parseFloat($("#JeunePourcent").val()) / -100 + 1;
+    if (pourcentCheck() == true && ratioCheck() == true) {
+        var jeune = parseFloat($("#JeunePourcent").val()) / 100;
+        var adulte = parseFloat($("#AdultePourcent").val()) / 100;
+        var aine = parseFloat($("#AinePourcent").val()) / 100;
 
-        $("#totalPourcentRabaisJeune").val(jeune * parseInt($("#totalBillet").val()))
+        var ratioJeune = parseFloat($("#RatioJeune").val()) / 100;
+        var ratioAdulte = parseFloat($("#RatioAdulte").val()) / 100;
+        var ratioAine = parseFloat($("#RatioAine").val()) / 100;
+
+        calculMoyenne();
+        calculTotalBillet();
+
+        $("#nbBilletJeune").val(TotalBillet * ratioJeune);
+        $("#nbBilletAdulte").val(TotalBillet * ratioAdulte);
+        $("#nbBilletAine").val(TotalBillet * ratioAine);
+
+        $("#rabaisBilletJeune").val(PrixMoyen * jeune);
+        $("#rabaisBilletAdulte").val(PrixMoyen * adulte);
+        $("#rabaisBilletAine").val(PrixMoyen * aine);
     }
 }//Calcule les rabais après avoir vérifié les données
